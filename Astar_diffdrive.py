fba021b97140bb0152721ae47ec0422295e2614a
Badri <badrinarayanan008@gmail.com>
2023-03-28 02:11:46 -0400

    Updated visualization and actions functions

--- Astar_diffdrive.py
+++ Astar_diffdrive.py
@@ -7,7 +7,7 @@ Authors : Vyshnav Achuthan (119304815)
 """
 import math
 import heapq
-#from obstacle_gen import obs_coord
+from obstacle_gen import obs_coord
 import pygame
 import numpy as np
 # Define the robot radius, clearance, tolerance, and goal tolerance
@@ -61,10 +61,7 @@ def reached_goal(node, goal):
     dx = node.state[0] - goal[0]
     dy = node.state[1] - goal[1]
     dist_to_goal = math.sqrt(dx**2 + dy**2)
-    if(node.state[2] == goal[2]):
-        return (dist_to_goal <= GOAL_TOLERANCE)
-    else:
-        return False
+    return (dist_to_goal <= GOAL_TOLERANCE)
 # Define get_cost function
 def get_cost(current_node, neighbor_node):
     return math.sqrt((current_node.x - neighbor_node.x)**2 + (current_node.y - neighbor_node.y)**2)
@@ -96,13 +93,12 @@ def next_node(node,Ul,Ur):
         Theta += (ROBOT_RADIUS/ROBOT_WHEEL_DIST)*(Ur-Ul) * dt
         X += dx
         Y += dy
-        Dist += math.sqrt(math.pow((0.5*ROBOT_RADIUS * (Ul + Ur) * math.cos(Theta) *
-                dt),2)+math.pow((0.5*ROBOT_RADIUS * (Ul + Ur) * math.sin(Theta) * dt),2))
+        Dist += math.sqrt(math.pow((0.5*ROBOT_RADIUS * (Ul + Ur) * math.cos(Theta)*dt),2)+math.pow((0.5*ROBOT_RADIUS * (Ul + Ur) * math.sin(Theta) * dt),2))
     Thetafin = 180 * (Theta)/math.pi
     return X,Y,Thetafin,Dist
 
 #Function to move forward
-def move_forward(node,goal,actions):
+def Left_purerot_RPM1(node,goal,actions):
     action = actions[0]
     new_x,new_y,new_theta,dist = next_node(node,action[0],action[1])
     if new_theta<0:
@@ -120,17 +116,18 @@ def move_forward(node,goal,actions):
     if is_valid_node(new_node):
         heapq.heappush(heap, (new_node))
     else:
-        print("Cannot move forward")
+        print("Cannot Rotate left with RPM1 speed")
 
 #Fuction to move 30 degrees to the left
-def turn_left_30(node,goal,step):
-    new_x = normalize(node.state[0] + step*math.cos(math.radians(node.state[2])+math.pi/6))
-    new_y = normalize(node.state[1] + step*math.sin(math.radians(node.state[2])+math.pi/6))
-    new_theta  = node.state[2] + 30
+def Right_purerot_RPM1(node,goal,actions):
+    action = actions[1]
+    new_x,new_y,new_theta,dist = next_node(node,action[0],action[1])
     if new_theta<0:
         new_theta +=360
     new_theta %= 360
-    new_cost_to_come = node.cost_to_come + step
+    new_x = normalize(new_x)
+    new_y = normalize(new_y)
+    new_cost_to_come = node.cost_to_come + dist
     new_cost_to_goal = math.sqrt((new_x - goal[0])**2 +(new_y - goal[1])**2)
     new_node = Node()
     new_node.state = [new_x,new_y,new_theta]
@@ -140,17 +137,18 @@ def turn_left_30(node,goal,step):
     if is_valid_node(new_node):
         heapq.heappush(heap, (new_node))
     else:
-        print("Cannot move 30 degrees to the left")
+        print("Cannot Rotate right with RPM2 speed")
 
 #Function to move 30 degrees to the right
-def turn_right_30(node,goal,step):
-    new_x = normalize(node.state[0] + step*math.cos(math.radians(node.state[2])+math.pi/6))
-    new_y = normalize(node.state[1] - step*math.sin(math.radians(node.state[2])+math.pi/6))
-    new_theta  = node.state[2] - 30
+def moveforward_RPM1(node,goal,actions):
+    action = actions[2]
+    new_x,new_y,new_theta,dist = next_node(node,action[0],action[1])
     if new_theta<0:
         new_theta +=360
     new_theta %= 360
-    new_cost_to_come = node.cost_to_come + step
+    new_x = normalize(new_x)
+    new_y = normalize(new_y)
+    new_cost_to_come = node.cost_to_come + dist
     new_cost_to_goal = math.sqrt((new_x - goal[0])**2 +(new_y - goal[1])**2)
     new_node = Node()
     new_node.state = [new_x,new_y,new_theta]
@@ -160,17 +158,18 @@ def turn_right_30(node,goal,step):
     if is_valid_node(new_node):
         heapq.heappush(heap, (new_node))
     else:
-        print("Cannot move 30 degrees to the right")
+        print("Cannot move forward with RPM1 speed")
 
 #Function to move 60 degrees to the left
-def turn_left_60(node,goal,step):
-    new_x = normalize(node.state[0] + step*math.cos(math.radians(node.state[2])+math.pi/3))
-    new_y = normalize(node.state[1] + step*math.sin(math.radians(node.state[2])+math.pi/3))
-    new_theta  = node.state[2] + 60
+def Left_purerot_RPM2(node,goal,actions):
+    action = actions[3]
+    new_x,new_y,new_theta,dist = next_node(node,action[0],action[1])
     if new_theta<0:
         new_theta +=360
     new_theta %= 360
-    new_cost_to_come = node.cost_to_come + step
+    new_x = normalize(new_x)
+    new_y = normalize(new_y)
+    new_cost_to_come = node.cost_to_come + dist
     new_cost_to_goal = math.sqrt((new_x - goal[0])**2 +(new_y - goal[1])**2)
     new_node = Node()
     new_node.state = [new_x,new_y,new_theta]
@@ -180,17 +179,18 @@ def turn_left_60(node,goal,step):
     if is_valid_node(new_node):
         heapq.heappush(heap, (new_node))
     else:
-        print("Cannot move 60 degrees to the left")
+        print("Cannot Rotate left with RPM2 speed")
 
 #Function to move 60 degrees to the right
-def turn_right_60(node,goal,step):
-    new_x = normalize(node.state[0] + step*math.cos(math.radians(node.state[2])+math.pi/3))
-    new_y = normalize(node.state[1] - step*math.sin(math.radians(node.state[2])+math.pi/3))
-    new_theta  = node.state[2] - 60
+def Right_purerot_RPM2(node,goal,actions):
+    action = actions[4]
+    new_x,new_y,new_theta,dist = next_node(node,action[0],action[1])
     if new_theta<0:
         new_theta +=360
     new_theta %= 360
-    new_cost_to_come = node.cost_to_come + step
+    new_x = normalize(new_x)
+    new_y = normalize(new_y)
+    new_cost_to_come = node.cost_to_come + dist
     new_cost_to_goal = math.sqrt((new_x - goal[0])**2 +(new_y - goal[1])**2)
     new_node = Node()
     new_node.state = [new_x,new_y,new_theta]
@@ -200,16 +200,77 @@ def turn_right_60(node,goal,step):
     if is_valid_node(new_node):
         heapq.heappush(heap, (new_node))
     else:
-        print("Cannot move 60 degrees to the right")
+        print("Cannot Rotate right with RPM2 speed")
+
+def moveforward_RPM2(node,goal,actions):
+    action = actions[5]
+    new_x,new_y,new_theta,dist = next_node(node,action[0],action[1])
+    if new_theta<0:
+        new_theta +=360
+    new_theta %= 360
+    new_x = normalize(new_x)
+    new_y = normalize(new_y)
+    new_cost_to_come = node.cost_to_come + dist
+    new_cost_to_goal = math.sqrt((new_x - goal[0])**2 +(new_y - goal[1])**2)
+    new_node = Node()
+    new_node.state = [new_x,new_y,new_theta]
+    new_node.parent = node
+    new_node.cost_to_come = new_cost_to_come
+    new_node.cost_to_goal = new_cost_to_goal
+    if is_valid_node(new_node):
+        heapq.heappush(heap, (new_node))
+    else:
+        print("Cannot Move forwrd with RPM2 speed")
+
+def Move_Rotate_RPM1_RPM2(node,goal,actions):
+    action = actions[6]
+    new_x,new_y,new_theta,dist = next_node(node,action[0],action[1])
+    if new_theta<0:
+        new_theta +=360
+    new_theta %= 360
+    new_x = normalize(new_x)
+    new_y = normalize(new_y)
+    new_cost_to_come = node.cost_to_come + dist
+    new_cost_to_goal = math.sqrt((new_x - goal[0])**2 +(new_y - goal[1])**2)
+    new_node = Node()
+    new_node.state = [new_x,new_y,new_theta]
+    new_node.parent = node
+    new_node.cost_to_come = new_cost_to_come
+    new_node.cost_to_goal = new_cost_to_goal
+    if is_valid_node(new_node):
+        heapq.heappush(heap, (new_node))
+    else:
+        print("Cannot move with left wheel RPM1 and right wheel RPM2 speed")
+
+def Move_Rotate_RPM2_RPM1(node,goal,actions):
+    action = actions[7]
+    new_x,new_y,new_theta,dist = next_node(node,action[0],action[1])
+    if new_theta<0:
+        new_theta +=360
+    new_theta %= 360
+    new_x = normalize(new_x)
+    new_y = normalize(new_y)
+    new_cost_to_come = node.cost_to_come + dist
+    new_cost_to_goal = math.sqrt((new_x - goal[0])**2 +(new_y - goal[1])**2)
+    new_node = Node()
+    new_node.state = [new_x,new_y,new_theta]
+    new_node.parent = node
+    new_node.cost_to_come = new_cost_to_come
+    new_node.cost_to_goal = new_cost_to_goal
+    if is_valid_node(new_node):
+        heapq.heappush(heap, (new_node))
+    else:
+        print("Cannot left wheel RPM2 and right wheel RPM1 speed")
+
 
 #Backtracking
 def backtrack(node):
     path = []
-    path.append(node.state)
+    path.append(node)
     print(node.state)
     while node.parent is not None:
         node = node.parent
-        path.append(node.state)
+        path.append(node)
         print(node.state)
     print("Done backtracking")
     visualize(path)
@@ -232,11 +293,14 @@ def astar(start, goal,actions):
         
             visited[int(curr_node.state[0]*2)][int(curr_node.state[1]*2)][int(curr_node.state[2]/30)] = 1
         
-            move_forward(curr_node,goal,actions)
-            turn_left_30(curr_node,goal,actions)
-            turn_right_30(curr_node,goal,actions)
-            turn_left_60(curr_node,goal,actions)
-            turn_right_60(curr_node,goal,actions)      
+            Left_purerot_RPM1(curr_node,goal,actions)
+            Right_purerot_RPM1(curr_node,goal,actions)
+            moveforward_RPM1(curr_node,goal,actions)
+            Left_purerot_RPM2(curr_node,goal,actions)
+            Right_purerot_RPM2(curr_node,goal,actions)
+            moveforward_RPM2(curr_node,goal,actions)
+            Move_Rotate_RPM1_RPM2(curr_node,goal,actions)
+            Move_Rotate_RPM2_RPM1(curr_node,goal,actions)      
     return None
 
 def visualize(path_gen): #Function to visualize the graph
@@ -256,6 +320,7 @@ def visualize(path_gen): #Function to visualize the graph
     CLEARANCE_COLOR = pygame.Color("white")
     VISITED_COLOR = pygame.Color("green")
     PATH_COLOR = pygame.Color("blue")
+    PIXEL_COLOR = pygame.Color("yellow")
 
     # Create the surface for the obstacle course
     surface = pygame.Surface((WINDOW_WIDTH, WINDOW_HEIGHT))
@@ -280,13 +345,20 @@ def visualize(path_gen): #Function to visualize the graph
     pygame.draw.polygon(surface, OBSTACLE_COLOR, ((460,225),(460,25),(510,125)))   
 
     for idx,any in enumerate(heap):
-        pygame.draw.rect(surface,VISITED_COLOR,(any.state[0],any.state[1],1,1))
-        window.blit(surface,(0,0))
-        # pygame.display.flip()
-        pygame.display.update()
+        if(any.parent is not None):
+            start_pos = (any.parent.state[0],any.parent.state[1])
+            print(start_pos)
+            end_pos = (any.state[0],any.state[1])
+            pygame.draw.line(surface,VISITED_COLOR,start_pos,end_pos,2)
+            pygame.draw.rect(surface,PIXEL_COLOR,(any.state[0],any.state[1],1,1))
+            window.blit(surface,(0,0))
+            # pygame.display.flip()
+            pygame.display.update()
     
-    for idx, every in enumerate(path_gen):
-        pygame.draw.rect(surface,PATH_COLOR,(every[0],every[1],5,5))
+    for idx,every in enumerate(path_gen):
+        if(every.parent is not None):
+            pygame.draw.line(surface,PATH_COLOR,(every.parent.state[0],every.parent.state[1]),(every.state[0],every.state[1]),2)
+            pygame.draw.rect(surface,PIXEL_COLOR,(every.state[0],every.state[1],1,1))
         # pygame.display.flip()
     # Blit the updated surface onto the Pygame window
         window.blit(surface, (0, 0))
@@ -361,8 +433,7 @@ if __name__ == "__main__":
     node.parent = None
     node.cost_to_come = 0
     node.cost_to_goal = 100000
-    step = int(input("Enter step between 1 and 10"))
     RPM1 = int(input("Enter lower limit RPM"))
     RPM2 = int(input("Enter higher limit RPM"))
-    actions = [[0,RPM1],[RPM1,0],[RPM1,RPM2],[0,RPM2],[RPM2,RPM2],[RPM1,RPM2],[RPM2,RPM1]]
+    actions = [[0,RPM1],[RPM1,0],[RPM1,RPM1],[0,RPM2],[RPM2,0],[RPM2,RPM2],[RPM1,RPM2],[RPM2,RPM1]]
     astar(node,goal,actions)
